34b50c70f20aa84a0c10e85c0297467ad8a98cf7
michaelstankiewiczcoding <129461748+michaelstankiewiczcoding@users.noreply.github.com>
2025-02-11 17:49:03 +0100

    Create websocketio.js
    const io = require("socket.io")(server, {
        cors: { origin: "*" },
    });
    
    io.on("connection", (socket) => {
        console.log("User connected:", socket.id);
    
        socket.on("sendMessage", (data) => {
            io.to(data.receiverId).emit("receiveMessage", data);
        });
    
        socket.on("disconnect", () => {
            console.log("User disconnected");
        });
    });

---
+++ websocketio.js
@@ -0,0 +1,15 @@
+const io = require("socket.io")(server, {
+    cors: { origin: "*" },
+});
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("sendMessage", (data) => {
+        io.to(data.receiverId).emit("receiveMessage", data);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
