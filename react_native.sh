d8f2aa8548e0a8fc32952509ea5036f9d1f5fea7
michaelstankiewiczcoding <129461748+michaelstankiewiczcoding@users.noreply.github.com>
2025-02-11 21:03:25 +0100

    Update
    npx react-native init DatingApp
    cd DatingApp
    npm install axios react-navigation react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context react-native-vector-icons socket.io-client @react-navigation/native @react-navigation/stack react-native-deck-swiper

---
+++ react_native.sh
@@ -0,0 +1,3 @@
+npx react-native init DatingApp
+cd DatingApp
+npm install axios react-navigation react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context react-native-vector-icons socket.io-client @react-navigation/native @react-navigation/stack react-native-deck-swiper
